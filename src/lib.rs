4321b939f6e2cbea5fb1f0b34547635e32e41672
Ben Simms <ben@bensimms.moe>
2019-06-27 09:47:55 +0100

    Correctly implement gc __clear__

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
-use std::collections::HashSet;
 use pyo3::{
+    create_exception,
     gc::{PyGCProtocol, PyVisit},
     mapping::PyMappingProtocol,
     prelude::*,
     PyTraverseError,
 };
+use std::collections::HashSet;
 
 use like_aho_corasick_but_different::SimpleFinder;
 
 #[pyclass]
 struct Searcher {
-    inner: Box<SimpleFinder<PyObject>>,
+    inner: Option<Box<SimpleFinder<PyObject>>>,
 }
+
+create_exception!(lacbd, SearcherDestructed, pyo3::exceptions::Exception);
 
 #[pymethods]
 impl Searcher {
@@ -19,7 +22,7 @@ impl Searcher {
     fn new(obj: &PyRawObject, patterns: Vec<(&str, PyObject)>) {
         obj.init({
             Searcher {
-                inner: Box::new(SimpleFinder::new(patterns)),
+                inner: Some(Box::new(SimpleFinder::new(patterns))),
             }
         })
     }
@@ -27,15 +30,17 @@ impl Searcher {
     fn __sizeof__(&self) -> PyResult<usize> {
         use std::mem::size_of;
 
-        Ok(self.inner.heap_bytes() + size_of::<PyObject>() * self.inner.data().len())
+        let inner = self.inner.as_ref().ok_or_else(|| SearcherDestructed)?;
+
+        Ok(inner.heap_bytes() + size_of::<PyObject>() * inner.data().len())
     }
 
     fn search(&self, haystack: &str) -> PyResult<Vec<PyObject>> {
         use pyo3::AsPyPointer;
 
-        let results: Vec<_> = self.inner.find_all(haystack)
-                                        .map(|(_, v)| v)
-                                        .collect();
+        let inner = self.inner.as_ref().ok_or_else(|| SearcherDestructed)?;
+
+        let results: Vec<_> = inner.find_all(haystack).map(|(_, v)| v).collect();
         let mut seen = HashSet::new();
 
         let gil = Python::acquire_gil();
@@ -54,25 +59,33 @@ impl Searcher {
 #[pyproto]
 impl PyMappingProtocol for Searcher {
     fn __len__(&self) -> PyResult<usize> {
-        Ok(self.inner.pattern_count())
+        let inner = self.inner.as_ref().ok_or_else(|| SearcherDestructed)?;
+
+        Ok(inner.pattern_count())
     }
 }
 
 #[pyproto]
 impl PyGCProtocol for Searcher {
     fn __traverse__(&self, visit: PyVisit) -> Result<(), PyTraverseError> {
-        for obj in self.inner.data().values() {
-            visit.call(obj)?;
+        if let Some(inner) = self.inner.as_ref() {
+            for obj in inner.data().values() {
+                visit.call(obj)?;
+            }
         }
+
         Ok(())
     }
 
     fn __clear__(&mut self) {
-        // ...
-        // I think this can just be a noop
-        // maybe if you do: l = []; s = Searcher([("a", l)]); l.append(s);
-        // it will create a cycle
-        // I say: don't do that...
+        if let Some(inner) = self.inner.take() {
+            let gil = Python::acquire_gil();
+            let py = gil.python();
+
+            for obj in inner.data().values() {
+                py.release(obj);
+            }
+        }
     }
 }
 
