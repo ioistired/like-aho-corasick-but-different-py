7e93e6af5cf349df62c63870dd3362989db66ce4
Benjamin Mintz <bmintz@protonmail.com>
2019-06-27 04:10:47 +0000

    bump patch version to 0.1.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name='lacbd',
-    version='0.1.4',
+    version='0.1.5',
     license='AGPL-3.0-or-later',
     description='Fast subsentence searching',
     long_description=long_description,
